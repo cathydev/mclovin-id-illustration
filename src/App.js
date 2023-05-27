5732ec8e620284a4a2a4d4d3e657ceb947286064
Catherine Mejias <catherinemdsilva@gmail.com>
2023-05-27 17:59:24 -0400

    adding grid

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="container">
       <div className="card">
         <div className="photo"></div>
-        <div style={{position: "relative", top: "10px", left: "3px"}}>
+        <div style={{ position: "relative", top: "10px", left: "3px" }}>
           <img src={Image} alt='barcode' width="160px" height="80px" />
         </div>
         <div className="info">
@@ -17,8 +17,20 @@ function App() {
               marginBottom: "15px"
             }}>Number 01-47-87441</div>
             <div>DOB 06/03/1981 EXP 06/03/2008</div>
-            <div>HT WT HAIR EYES SEX CTY</div>
-            <div>5-10 130 BRO M O</div>
+            <div class="angry-grid">
+              <div id="item-0">HT</div>
+              <div id="item-1">WT</div>
+              <div id="item-2">HAIR</div>
+              <div id="item-3">EYES</div>
+              <div id="item-4">SEX</div>
+              <div id="item-5">CTY</div>
+              <div id="item-6">5-10</div>
+              <div id="item-7">130</div>
+              <div id="item-8">BRO</div>
+              <div id="item-9">BRO</div>
+              <div id="item-10">M</div>
+              <div id="item-11">O</div>
+            </div>
             <div>ISSUE DATE CLASS RESTR ENDORSE</div>
             <div>06/18/1998 3</div>
           </div>
