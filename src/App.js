9775220cd3caf3d825714d15240e0a54056bfd62
Catherine Mejias <catherinemdsilva@gmail.com>
2023-05-27 13:30:23 -0400

    Adding barcode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import Image from "./barcode.svg";
 
 function App() {
   return (
     <div className="container">
       <div className="card">
         <div className="photo"></div>
+        <div style={{position: "relative", top: "10px", left: "8px"}}>
+          <img src={Image} alt='barcode' width="160px" height="80px" />
+          <span>McLOVIN</span>
+        </div>
         <div className="info">
           <span className='state'>HAWAII</span> <span className='text'>driver license</span>
+          <div className='id-data'>
+            <div style={{
+              fontSize: "small",
+              marginBottom: "15px"
+            }}>Number 01-47-87441</div>
+            <div>DOB 06/03/1981 EXP 06/03/2008</div>
+            <div>HT WT HAIR EYES SEX CTY</div>
+            <div>5-10 130 BRO M O</div>
+            <div>ISSUE DATE CLASS RESTR ENDORSE</div>
+            <div>06/18/1998 3</div>
+          </div>
         </div>
       </div>
     </div>
